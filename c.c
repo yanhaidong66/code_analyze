0e922df5e667ddb5a66fe19493c96a6717b6da5f
hd <1178258121@qq.com>
2023-11-03 19:32:51 +0800

    finish my work

--- c.c
+++ c.c
@@ -1,4 +1,6 @@
-#include<stdio.h>
-int add(int a, int b) {
-	return a + b;
+int main(){
+  int a=34;
+  char apple[20]="apple";
+  int b  = 32454;
+	printf("%d",a+b);
 }
--- code_compiler.cpp
+++ code_compiler.cpp
@@ -1,20 +1,97 @@
 #include<iostream>
-#include<string>
+#include<fstream>
+#include"MyHashMap.cpp"
 using namespace std;
 class codeCompiler {
 public:
-	char getCode();
-	bool codeFetch(HashTable hashTable,string fileName);
+	HashMap hm;
+	bool getCode(const char*);
+	bool getCode(const char);
+	bool codeFetch(string fileName);
+	bool codeOver(char c);
 };
 
-char codeCompiler::getCode()
+bool codeCompiler::getCode(const char* c)
 {
-
-	return 0;
+	string s(c);
+	hm.put(s, 1);
+	return true;
 }
 
-bool codeCompiler::codeFetch(HashTable hashTable, string fileName)
+bool codeCompiler::getCode(const char c)
 {
+	string s(c+"");
+	hm.put(s, 1);
+	return true;
+}
 
+bool codeCompiler::codeFetch( string fileName)
+{
+	char* buf=new char[200];
+	ifstream file(fileName.c_str());
+	if (!file) {
+		std::cerr << "文件无法打开" << std::endl;
+		return 1;
+	}
+	if (file) {
+
+		while (file.eof()==false) {
+			int size = 0;
+			char c = 0;
+			file.get(c);
+			buf[size++] = c;
+			if (codeOver(c) == true) {
+				getCode(c);
+				cout << c;
+				cout << endl;
+				continue;
+			}
+		
+			while(codeOver(c)==false) {
+				file.get(c);
+				if (codeOver(c) == false)
+					buf[size++] = c;
+				else
+					break;
+					
+			}
+			buf[size] = '\0';
+			getCode(buf);
+			cout << buf ;
+			cout << endl;
+			getCode(c);
+			cout << c;
+			cout << endl;
+			
+			
+		}
+		file.close();
+
+	}
+
+
+	file.close();
 	return false;
+}
+
+bool codeCompiler::codeOver(const char c)
+{
+	if (c >= 'A' && c <= 'Z'|| c >= 'a' && c <= 'z' || c>='0'&&c<='9')
+		return false;
+	return true;
+}
+
+
+
+int main() {
+	codeCompiler codeC;
+	codeC.codeFetch("c.c");
+	string keys[200];
+	int values[200];
+	int size=codeC.hm.getAll(keys, values, 200);
+	cout << "size:" << size << endl;
+	for (int i = 0; i < size; i++) {
+		cout << "key:" << keys[i];
+		cout << "           value:" << values[i]<< endl;
+	}
 }
